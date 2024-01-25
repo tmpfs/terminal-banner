a9cde1f6db977a580be8a9c7ab4cbf676c37eb1c
ZhengJin <fangzhengjin@gmail.com>
2024-01-25 19:10:13 +0800

    fix: update example

--- examples/default.rs
+++ examples/default.rs
@@ -6,12 +6,12 @@ fn main() {
         .width(82)
         .symbols(BoxSymbols::default())
         .text(Text::from("DEFAULT"))
-        .text(Text::from("LEFT").align(TextAlign::LEFT).color(Color::Green))
-        .text(Text::from("CENTER").align(TextAlign::CENTER).color(Color::Blue))
-        .text(Text::from("RIGHT").align(TextAlign::RIGHT))
+        .text(Text::from("LEFT").align(TextAlign::LEFT).color(Color::BrightGreen))
+        .text(Text::from("CENTER").align(TextAlign::CENTER).color(Color::BrightBlue))
+        .text(Text::from("RIGHT").align(TextAlign::RIGHT).color(Color::BrightRed))
         .padding(Padding::one())
         .divider()
-        .text(Text::from("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
+        .text(Text::from("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.").color(Color::BrightCyan))
         .render();
     println!("{}", banner);
 }
--- examples/strong.rs
+++ examples/strong.rs
@@ -1,3 +1,4 @@
+use colored::Color;
 use terminal_banner::{Banner, BoxSymbols, Padding, Text, TextAlign};
 
 fn main() {
@@ -6,11 +7,11 @@ fn main() {
         .symbols(BoxSymbols::strong())
         .padding(Padding::one())
         .text(Text::from("DEFAULT"))
-        .text(Text::from("LEFT").align(TextAlign::LEFT))
-        .text(Text::from("CENTER").align(TextAlign::CENTER))
-        .text(Text::from("RIGHT").align(TextAlign::RIGHT))
+        .text(Text::from("LEFT").align(TextAlign::LEFT).color(Color::BrightGreen))
+        .text(Text::from("CENTER").align(TextAlign::CENTER).color(Color::BrightBlue))
+        .text(Text::from("RIGHT").align(TextAlign::RIGHT).color(Color::BrightRed))
         .divider()
-        .text(Text::from("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
+        .text(Text::from("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.").color(Color::BrightCyan))
         .render();
     println!("{}", banner);
 }
